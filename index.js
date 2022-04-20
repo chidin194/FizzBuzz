dbbfdbfe11803ad36a912b7276c0fcb7777660d9
chidin194 <chiara.dinan@softwire.com>
2022-04-20 14:22:49 +0100

    FizzBuzz: Add rules for multiples of 3 and 5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // FizzBuzz first commit
+
+for(let i=1; i<=100; i++) {
+
+    let result = [];
+
+    if(i % 3 === 0)
+        result.push("Fizz")
+
+    if(i % 5 === 0)
+        result.push("Buzz")
+
+    if (result.length === 0) {
+        result.push(i.toString());
+    }
+
+    console.log(result.join(""));
+}
